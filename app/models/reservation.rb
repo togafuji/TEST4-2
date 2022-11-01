bd939fed0962ada6d9075b1e11657874955d864b
togafuji <happy.tennis.ty.tm@gmail.com>
2022-11-01 15:14:00 +0000

    ログイン画像、開始日の遡りを修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,6 +8,7 @@ class Reservation < ApplicationRecord
   validate :errors_matter
   def errors_matter
     errors.add(:start_date, "は終了日以前の日付にしてください") if start_date > end_date
+    errors.add(:start_date, "は本日以降の日付にしてください") if start_date.strftime('%Y-%m-%d') < Date.current.strftime('%Y-%m-%d')
     errors.add(:people, "は一人以上にしてください") if people < 1 
   end
 end
